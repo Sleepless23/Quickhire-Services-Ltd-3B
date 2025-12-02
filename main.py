ceeca43de7dba7f1acc08b43c8cb9f391a286f72
Alvin Castro <castroalvinjr64@gmail.com>
2025-12-02 17:58:36 +0800

    Created a function to register employees

--- main.py
+++ main.py
@@ -27,17 +27,35 @@ class Employee:
             "contact": self.contact
         }
 
-    #funtion to load data
-    def load_data(filename):
-        try:
-            with open(filename, "r") as file:
-                return json.load(file)
-        except:
-            return []
-        
-    #function to save data
-    def save_data(filename, data):
-        with open(filename, "w") as file:
-            json.dump(data, file)
+#funtion to load data
+def load_data(filename):
+    try:
+        with open(filename, "r") as file:
+            return json.load(file)
+    except:
+        return []
+    
+#function to save data
+def save_data(filename, data):
+    with open(filename, "w") as file:
+        json.dump(data, file)
+
+#funtion to register employees
+def register_employees():
+    employees = load_data(employee_file)
+    emp_id = len(employees) + 1
+
+    print("\n---Register New Employee---")
+    name = input("Name: ")
+    role = input("Role: ")
+    dept = input("Department: ")
+    rate = float(input("Hourly Rate: "))
+    contact = input("Contact: ")
+
+    emp = Employee(emp_id,name, role, dept, rate, contact)
+    employees.append(emp.my_dict)
+
+    save_data(employee_file, employees)
+    print("Employee Registered Successfully")
 
 
